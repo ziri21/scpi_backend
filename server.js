402de384f9d8252717228b60bb8db7c00f88d7dc
thiziri <raguebthiziri@gmail.com>
2026-02-16 08:23:47 +0100

    correction serveur

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ const connectDB = require("./config/db.js");
 const app = express();
 app.use(cors());
 app.use(express.json());
-try {
-  connectDB();
-  app.use("/admin", adminRouter);
-  app.use("/societeGestion", SGRouter);
-  app.use("/scpi", scpiRouter);
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`le serveur est lancé sur le port :${PORT}`);
-  });
-} catch (err) {
-  console.error(err.message);
-}
+const lancer = async () => {
+  try {
+    await connectDB();
+    app.use("/admin", adminRouter);
+    app.use("/societeGestion", SGRouter);
+    app.use("/scpi", scpiRouter);
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`le serveur est lancé sur le port :${PORT}`);
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+lancer();
